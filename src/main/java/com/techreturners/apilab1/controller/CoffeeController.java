76a7243622a0a180d31731204370f19a7e1f8396
Deepana Naidu <deepana.naidu@gmail.com>
2022-05-03 22:27:50 +0100

    coffeeLover() method added in coffee controller for GET endpoint and url mapping ("/coffeelover)"

--- src/main/java/com/techreturners/apilab1/controller/CoffeeController.java
+++ src/main/java/com/techreturners/apilab1/controller/CoffeeController.java
@@ -1,4 +1,13 @@
 package com.techreturners.apilab1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CoffeeController {
+    @GetMapping(value = "/coffeelover")
+    public String coffeeLover(){
+        return "I like coffee!";
+    }
+
 }
